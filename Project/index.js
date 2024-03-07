3ed8f1c242fd12b1c986c9e7069bc5cab922c62b
Felipe Lucas <felipea.lucas@outlook.com>
2024-03-07 03:15:32 -0300

    adding npm and eslint in project

--- Project/index.js
+++ Project/index.js
@@ -1,12 +1,13 @@
 //Standard pattern
-function helloWorld(){
-    console.log("Hello World");
+function helloWorld() {
+    console.log("Hello World")
 }
 helloWorld();
 
 //ECMAScript 2015 - ES6 - ARROW FUNCTION
 const greeting = () => {
-    var date = new Date();
-    return date.getHours() <= 12? "Good morning": date.getHours() <= 18? "Good afternoon": "Good evening";
+    // var date = new Date();
+    const date2 = new Date();
+    return date2.getHours() <= 12 ? "Good morning" : date2.getHours() <= 18 ? "Good afternoon" : "Good evening";
 }
 console.log("The greeting of the moment is: " + greeting());
