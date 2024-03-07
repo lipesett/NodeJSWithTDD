965f4c55b504e49f5e920c85a3e4e0756d77e2d4
Felipe Lucas <felipea.lucas@outlook.com>
2024-03-07 03:20:37 -0300

    refactor code

--- Project/index.js
+++ Project/index.js
@@ -2,12 +2,14 @@
 function helloWorld() {
     console.log("Hello World")
 }
-helloWorld();
 
 //ECMAScript 2015 - ES6 - ARROW FUNCTION
 const greeting = () => {
-    // var date = new Date();
-    const date2 = new Date();
-    return date2.getHours() <= 12 ? "Good morning" : date2.getHours() <= 18 ? "Good afternoon" : "Good evening";
-}
+    const date = new Date().getHours();
+    if (date < 12) return "Good morning";
+    if (date < 18) return "Good afternoon";
+    return "Good night";
+};
+
+helloWorld();
 console.log("The greeting of the moment is: " + greeting());
