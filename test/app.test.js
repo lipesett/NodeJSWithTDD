1117cc2d562af5a2db55c8c80db56e64a04071da
Felipe Lucas <felipea.lucas@outlook.com>
2024-03-10 01:29:45 -0300

    creating the 'app.test.js' and 'app.js' files

---
+++ test/app.test.js
@@ -0,0 +1,17 @@
+// ALL TESTS RELATED TO THE APP WILL BE HERE
+const request = require('supertest');
+
+const app = require('../src/app.js');
+
+/* 
+THE SUPERTEST MUST BUILD THE APP AND SERVER;
+FIRST WE MUST MAKE A REQUEST TO THE SERVER;
+AFTERWARD, WE MUST CATCH THE RESPONSE STATUS CODE;
+IN THE END, WE MUST COMPARE THE RETURN WITH OUR EXPECT;
+ */
+test('Must response at the root', () => {
+    return request(app).get('/')
+        .then(res => {
+            expect(res.statusCode).toBe(200);
+        });
+});
