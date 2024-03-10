39fe2775a9f9dc14a17dede9b04962955f03757d
Felipe Lucas <lipeseta12@gmail.com>
2024-03-10 03:02:36 -0300

    documenting the test

--- test/user.test.js
+++ test/user.test.js
@@ -1,7 +1,16 @@
+// ALL TESTS RELATED TO THE USERS WILL BE HERE!
 const request = require('supertest');
 
 const app = require('../src/app');
 
+/* 
+WE MUST RETURN ALL USERS;
+FIRST WE MUST MAKE A REQUEST FOR THE ROUTE '/users';
+WE MUST CAPTURE AND COMPARE THE 'statusCode' OF THE RESPONSE;
+WE MUST CHECK THE SIZE OF THE ARRAY;
+WE MUST CHECK IF THE RESPONSE BODY CONTAINS THE PROPERTY
+"name" WITH VALUE "Felipe Lucas";
+ */
 test('Must get all users', () => {
     return request(app).get('/users')
         .then(res => {
