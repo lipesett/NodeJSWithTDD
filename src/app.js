f73f6abe887de307dff1ffc864a06468a13c1804
Felipe Lucas <lipeseta12@gmail.com>
2024-03-10 02:51:33 -0300

    adding english notes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,16 @@ const app = require('express')();
 
 app.get('/', (req, res) => {
     res.status(200).send();
-})
+});
+
+app.get('/users', (req, res) => {
+    const users = [
+        {
+            "name": "Felipe Lucas",
+            "email": "felipe@email.com"
+        }
+    ];
+    res.status(200).json(users);
+});
 
 module.exports = app;
