0161070a574a4f0b8a7816561d7892f109ac237c
Felipe Lucas <felipea.lucas@outlook.com>
2024-03-11 19:50:19 -0300

    creating 'routes.js' file

---
+++ src/config/routes.js
@@ -0,0 +1,9 @@
+/*
+HERE WE SEE HOW EASY IT WILL BE TO EXPORT NEW ROUTES
+FROM THE FILE 'USERS' WHEN THEY ARE CREATED.
+*/
+module.exports = (app) => {
+    app.route('/users')
+        .get(app.routes.users.getAll)
+        .post(app.routes.users.create)
+}
