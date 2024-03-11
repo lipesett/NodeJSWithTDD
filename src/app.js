6f0748488f21b6513a7fa93ecb03087209c1f696
Felipe Lucas <felipea.lucas@outlook.com>
2024-03-11 19:51:58 -0300

    extracting the routes from 'app.js' to the routes file 'user.js'

--- src/app.js
+++ src/app.js
@@ -3,27 +3,13 @@ const consign = require('consign');
 
 consign({ cwd: 'src', verbose: false })
     .include('./config/middlewares.js')
+    .then('./routes')
+    .then('./config/routes.js')
     .into(app);
 
 // HOME PAGE
 app.get('/', (req, res) => {
     res.status(200).send();
-});
-
-// GET ALL USERS
-app.get('/users', (req, res) => {
-    const users = [
-        {
-            "name": "Felipe Lucas",
-            "email": "felipe@email.com"
-        }
-    ];
-    res.status(200).json(users);
-});
-
-// POST USER
-app.post('/users', (req, res) => {
-    res.status(201).json(req.body);
 });
 
 module.exports = app;
