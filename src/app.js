aeeb10ef966d57f9b5fbef9508dffe71f1361d5e
Felipe Lucas <felipea.lucas@outlook.com>
2024-03-16 18:51:40 -0300

    adding knex-logger on context

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ const app = require('express')();
 const consign = require('consign');
 const knex = require('knex');
 const knexfile = require('../knexfile')
+const knexLogger = require('knex-logger');
 
 // TODO create dynamic switching
 app.db = knex(knexfile.test);
+app.use(knexLogger(app.db));
 
 consign({ cwd: 'src', verbose: false })
     .include('./config/middlewares.js')
