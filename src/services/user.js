b27e5537d9c694c9012ae562b6d1f0a0047d7c60
Felipe Lucas <felipea.lucas@outlook.com>
2024-03-22 00:15:28 -0300

    adding filter to 'getAll' and adding a new validation for 'create'

--- src/services/user.js
+++ src/services/user.js
@@ -1,12 +1,16 @@
 module.exports = (app) => {
-    const getAll = () => {
-        return app.db('users').select();
+    const getAll = (filter = {}) => {
+        return app.db('users').where(filter).select();
     };
 
     const create = async (user) => {
         if (!user.name) return { error: 'Name is required' };
         if (!user.email) return { error: 'Email is required' };
         if (!user.password) return { error: 'Password is required' };
+
+        const userDb = await getAll({ email: user.email });
+        if (userDb && userDb.length > 0) return { error: 'There is already a user with this email' };
+
         return app.db('users').insert(user, '*');
     };
 
